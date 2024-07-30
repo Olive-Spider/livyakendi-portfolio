9b0aa04cf5b1c55f1aab88e772ef971a564307fb
Livya Kendi Kiruja <livyakendikiruja@gmail.com>
2024-07-30 16:50:34 +0300

    video background

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,14 @@ import Skills from "./skills/Skills.jsx";
 import Projects from "./projects/Projects.jsx";
 import Contact from "./contact/Contact.jsx";
 import "./styles/app.css";
-// import Background from "./background/Background.js";
+import Background from "./background/Background.jsx";
 // import PlayerStats from "./playerStats/PlayerStats.js";
 
 const App = () => {
   return (
     <Router>
       <Nav />
-      {/* <Background /> */}
+      <Background />
       <Routes>
         <Route path="/" element={<About />} />
         <Route path="/skills" element={<Skills />} />
