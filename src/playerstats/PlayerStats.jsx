7a1536942327a5746468386e9acb74ce3ad42151
Livya Kendi Kiruja <livyakendikiruja@gmail.com>
2024-08-01 00:28:17 +0300

    hiring button and resume

--- src/playerstats/PlayerStats.jsx
+++ src/playerstats/PlayerStats.jsx
@@ -1,4 +1,21 @@
 // import React from "react";
+// import "../styles/playerStats.css";
+
+// const PlayerStats = () => {
+//   return (
+//     <div id="playerStats">
+//       <h1>LIVYA KENDI</h1>
+//       <div id="playerStats__lines">
+//         <span id="playerStats__lines__thick"></span>
+//         <span id="playerStats__lines__thin"></span>
+//       </div>
+//       <h2>Fullstack Developer</h2>
+//     </div>
+//   );
+// };
+
+// export default PlayerStats;
+
 import "../styles/playerStats.css";
 
 const PlayerStats = () => {
@@ -10,10 +27,22 @@ const PlayerStats = () => {
         <span id="playerStats__lines__thin"></span>
       </div>
       <h2>Fullstack Developer</h2>
-      <br />
-      <div>
-        <a href="/Livya Kendi Resume.pdf" download="Livya Kendi Resume.pdf" target="_blank">
-          <button id="button">Download Resume</button>
+      <div id="playerStats__buttons">
+        <a 
+          href="https://resume.showwcase.com/olive-spider/6681.pdf?v=1722459885997" 
+          target="_blank" 
+          rel="noopener noreferrer" 
+          className="playerStats__button"
+        >
+          Download Resume
+        </a>
+        <a 
+          href="https://www.upwork.com/freelancers/~015d499ee4405b621a?mp_source=share" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          className="playerStats__button"
+        >
+          Hire Me
         </a>
       </div>
     </div>
