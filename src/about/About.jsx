f1afcc54f2ebdf3c0197c466dc254273d58788f1
Livya Kendi Kiruja <livyakendikiruja@gmail.com>
2024-07-31 14:50:47 +0300

    about section and skills

--- src/about/About.jsx
+++ src/about/About.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import { Component } from "react";
 import Avatar from "../avatar/Avatar.jsx";
 import AboutMenu from "./AboutMenu.jsx";
 
--- src/skills/Skills.jsx
+++ src/skills/Skills.jsx
@@ -1,9 +1,14 @@
-// import React from 'react'
+import { Component } from "react";
+import Avatar from "../avatar/Avatar.jsx";
+import SkillsMenu from "./SkillsMenu.jsx";
 
-export default function Skills() {
-  return (
-    <div>
-      
-    </div>
-  )
+export default class About extends Component {
+  render() {
+    return (
+      <>
+        <Avatar page="skills" />
+        <SkillsMenu />
+      </>
+    );
+  }
 }
